3d29df0d2b81c1e0915142f87dc70558791e7807
Stefan Bols <stefan@imbols.net>
2018-03-09 15:40:28 +0100

    Init

---
+++ tourofheroes/src/app/app-routing.routing.ts
@@ -0,0 +1,21 @@
+// import { Routes, RouterModule } from '@angular/router';
+
+// const routes: Routes = [
+//   {  },
+// ];
+
+// export const AppRoutingRoutes = RouterModule.forChild(routes);
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
+
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
---
+++ tourofheroes/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.routing';
+
+// COMPONENTS
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+
+// SERVICES
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [
+    HeroService,
+    MessageService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
